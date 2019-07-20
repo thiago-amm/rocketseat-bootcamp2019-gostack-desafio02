bee02e993dbb3facedf253fc39747106584b58b9
thiago-amm <thiago-amm@outlook.com>
2019-07-20 11:06:25 -0300

    Corrigida a importação do databaseConfig.

--- meetapp/src/database/index.js
+++ meetapp/src/database/index.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
-import databaseCofnig from '../config/database';
+import databaseConfig from '../config/database';
 
 const models = [User];
 

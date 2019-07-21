cffb07dd0a8c71f921ac4792e8a304ee5eccc996
thiago-amm <thiago-amm@outlook.com>
2019-07-21 18:23:27 -0300

    Criado controller de usuários.

---
+++ meetapp/src/app/controllers/UserController.js
@@ -0,0 +1,27 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ error: 'Já existe um usuário cadastrado com esse e-mail!' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();

3a34b7c78cd1005d1c3c327c5f93850472550a7d
thiago-amm <thiago-amm@outlook.com>
2019-07-21 18:24:24 -0300

    Criada rota para o controller de usuários.

--- meetapp/src/routes.js
+++ meetapp/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Thiago Alexandre Martins Monteiro',
-    email: 'thicmp@gmail.com',
-    password_hash: 'thiago123',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;

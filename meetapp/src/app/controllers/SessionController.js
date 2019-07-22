e492c0fcc0b8dd880c7e5219bbec5c2132d1f192
thiago-amm <thiago-amm@outlook.com>
2019-07-22 01:08:43 -0300

    Implementada autenticação com JWT e troca de senha do usuário.

--- meetapp/src/app/controllers/SessionController.js
+++ meetapp/src/app/controllers/SessionController.js
@@ -1,7 +1,36 @@
+import jwt from 'jsonwebtoken';
+
 import User from '../models/User';
 
+import authConfig from '../../config/auth';
+
 class SessionController {
-  async store(req, res) {}
+  async store(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    // 1. Verifica se o usuário já foi cadastrado.
+    if (!user) {
+      return res.status(401).json({ error: 'Usuário não encontrado!' });
+    }
+    // 2. Verifica se a senha informada está correta.
+    if (!(await user.checkPassword(password))) {
+      return res
+        .status(401)
+        .json({ error: 'A senha informada está incorreta!' });
+    }
+    const { id, name } = user;
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      // Payload, Private Key
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
 }
 
 export default new SessionController();
---
+++ meetapp/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'db84a159f642444806f7b7a6f2588d15',
+  expiresIn: '7d',
+};
